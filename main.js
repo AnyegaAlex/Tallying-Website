e2a289e03131df24880438917d13bbc7630af83a
anyega <anyega.alex.kamau@gmail.com>
2023-03-20 02:10:31 +0300

    Basic Functionality of Website

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 let count = 0
 
 function increment(){
-  count = count + 1
+  count += 1
   document.getElementById("count_el").innerHTML = count
 }
 
 function save(){
+
   console.log(count)
+
+  let saveEl = document.getElementById("save_el")
+  let countDashes = count + " - "
+
+  saveEl.innerHTML += countDashes
 }
+
+
+
 
 
